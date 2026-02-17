a07a5717f02d698663d8d1fabf09d092f3d07424
Elia <elia.mmoral@gmail.com>
2026-02-17 17:41:05 +0100

    Main classes added and changed MainActivity.kt and added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 android {
     namespace = "com.unirfp.app_retrofit_elia_martinez"
     compileSdk {
-        version = release(36)
+        version = release(35)
     }
 
     defaultConfig {
         applicationId = "com.unirfp.app_retrofit_elia_martinez"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -53,4 +53,18 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("androidx.compose.ui:ui:1.6.1")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
+
+    // Añadimos Coil para cargar imágenes
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
